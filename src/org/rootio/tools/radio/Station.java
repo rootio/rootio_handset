fd5539f03c26d584ceaa58a06a70b9b387f72779
Jude Love Mukundane <jude19love@gmail.com>
2014-03-21 12:41:27 +0300

    Multicast announcement of stream host possible

--- src/org/rootio/tools/radio/Station.java
+++ src/org/rootio/tools/radio/Station.java
@@ -1,6 +1,9 @@
 package org.rootio.tools.radio;
 
+import java.net.InetAddress;
+
 import org.rootio.tools.persistence.DBAgent;
+import org.rootio.tools.utils.Utils;
 
 import android.content.Context;
 
@@ -12,6 +15,8 @@ public class Station {
 	private String name;
 	private StationStatus stationStatus;
 	private Context context;
+	private InetAddress multicastIPAddress;
+	private int multicastPort;
 
 	public Station(Context context) {
 		this.context = context;
@@ -41,10 +46,20 @@ public class Station {
 	public StationStatus getStationStatus() {
 		return this.stationStatus;
 	}
+	
+	public InetAddress getMulticastIPAddress()
+	{
+		return this.multicastIPAddress;
+	}
+	
+	public int getMulticastPort()
+	{
+		return this.multicastPort;
+	}
 
 	private void loadStationInfo() {
 		String tableName = "station";
-		String[] columnsToFetch = new String[] { "location", "owner", "telephonenumber", "name", "frequency" };
+		String[] columnsToFetch = new String[] { "location", "owner", "telephonenumber", "name", "frequency", "multicastipaddress", "multicastport" };
 		DBAgent dbAgent = new DBAgent(this.context);
 		String[][] stationDetails = dbAgent.getData(true, tableName, columnsToFetch, null, null, null, null, null, null);
 		if (stationDetails.length > 0) {
@@ -52,11 +67,9 @@ public class Station {
 			this.owner = stationDetails[0][1];
 			this.telephoneNumber = stationDetails[0][2];
 			this.name = stationDetails[0][3];
-			try {
-				this.frequency = Float.parseFloat(stationDetails[0][4]);
-			} catch (NumberFormatException ex) {
-				this.frequency = 0;
-			}
+			this.multicastPort = Utils.parseIntFromString(stationDetails[0][6]);
+			this.frequency = Utils.parseFloatFromString(stationDetails[0][4]);
+			this.multicastIPAddress = Utils.parseInetAddressFromString(stationDetails[0][5]);
 		}
 	}
 
