1104bb99994571c284e2e2eecf0a9105a5d15e91
Jude Love Mukundane <jude19love@gmail.com>
2014-04-14 17:35:42 +0300

    Decorator for services with serviceID and running info

---
+++ src/org/rootio/services/ServiceInformationPublisher.java
@@ -0,0 +1,18 @@
+package org.rootio.services;
+
+public interface ServiceInformationPublisher {
+
+	/**
+	 * Gets whether or not the service is running
+	 * @return Boolean indicating if service is running or not. True: running, False: not running
+	 */
+	public boolean isRunning();
+	
+	/**
+	 * Gets the ID of the service
+	 * @return Integer representing ID of the service
+	 */
+	public int getServiceId();
+	
+	
+}
