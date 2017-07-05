b8a43ddcaab99ab09a40cbcceb3f6f74ec45641b
Jude Mukundane <jude19love@gmail.com>
2017-07-05 16:45:34 +0100

    Moved source files to app/

--- src/org/rootio/activities/services/ServiceExitInformable.java
+++
@@ -1,9 +0,0 @@
-package org.rootio.activities.services;
-
-public interface ServiceExitInformable {
-
-	/**
-	 * Ends the connection by an activity to the program service allowing the program service to shut down.
-	 */
-	public void disconnectFromRadioService();
-}
--- src/org/rootio/tools/media/MediaType.java
+++
@@ -1,9 +0,0 @@
-package org.rootio.tools.media;
-/**
- * Defines the Types of media that can be played during a program schedule
- * @author HP Envy
- *
- */
-public enum MediaType {
-File, Stream
-}
