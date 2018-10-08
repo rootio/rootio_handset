f091ebffa86f07c7433503bf9db89892445b93f7
Jude Love <jude17love@hotmail.com>
2018-10-08 14:21:01 +0100

    Moved Synchronization looping to Handlers with delayed processing

--- app/src/main/java/org/rootio/services/synchronization/SynchronizationDaemon.java
+++ app/src/main/java/org/rootio/services/synchronization/SynchronizationDaemon.java
@@ -11,19 +11,21 @@ import org.rootio.tools.utils.Utils;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.os.Handler;
 import android.util.Log;
 
 public class SynchronizationDaemon implements Runnable {
     private final Context parent;
     private final Cloud cloud;
+    private Handler handler;
 
     @Override
     public void run() {
 
-        while (((SynchronizationService) this.parent).isRunning()) {
+       /* while (((SynchronizationService) this.parent).isRunning()) {
             // turn on mobile data and wait
             //this.toggleData(true);
-            this.getSomeSleep(15000);
+            //this.getSomeSleep(15000);
 
             // do the sync
             this.synchronize(new DiagnosticsHandler(this.parent, this.cloud));
@@ -35,18 +37,69 @@ public class SynchronizationDaemon implements Runnable {
             this.synchronize(new StationHandler(this.parent, this.cloud));
             this.synchronize(new MusicListHandler(this.parent, this.cloud));
             this.synchronize(new PlaylistHandler(this.parent, this.cloud));
-
             // turn off mobile data
             //this.toggleData(false);
 
-            this.getSomeSleep(this.getFrequency() * 1000); // seconds to milliseconds
+            this.getSomeSleep(this.getFrequency() * 1000); // seconds to milliseconds*/
+        this.synchronize();
 
-        }
     }
+
+    private void synchronize() {
+        this.handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Thread thread = new Thread(new Runnable() {
+                    public void run() {
+                        SynchronizationDaemon.this.
+
+                                synchronize(new DiagnosticsHandler(SynchronizationDaemon.this.parent, SynchronizationDaemon.this.cloud));
+                        SynchronizationDaemon.this.
+
+                                synchronize(new ProgramsHandler(SynchronizationDaemon.this.parent, SynchronizationDaemon.this.cloud));
+                        SynchronizationDaemon.this.
+
+                                synchronize(new CallLogHandler(SynchronizationDaemon.this.parent, SynchronizationDaemon.this.cloud));
+                        SynchronizationDaemon.this.
+
+                                synchronize(new SMSLogHandler(SynchronizationDaemon.this.parent, SynchronizationDaemon.this.cloud));
+                        SynchronizationDaemon.this.
+
+                                synchronize(new WhitelistHandler(SynchronizationDaemon.this.parent, SynchronizationDaemon.this.cloud));
+                        SynchronizationDaemon.this.
+
+                                synchronize(new FrequencyHandler(SynchronizationDaemon.this.parent, SynchronizationDaemon.this.cloud));
+                        SynchronizationDaemon.this.
+
+                                synchronize(new StationHandler(SynchronizationDaemon.this.parent, SynchronizationDaemon.this.cloud));
+                        SynchronizationDaemon.this.
+
+                                synchronize(new MusicListHandler(SynchronizationDaemon.this.parent, SynchronizationDaemon.this.cloud));
+                        SynchronizationDaemon.this.
+
+                                synchronize(new PlaylistHandler(SynchronizationDaemon.this.parent, SynchronizationDaemon.this.cloud));
+                    }
+                });
+                thread.start();
+                try {
+                    //if this sync is taking long, wait for at most 3 sync cycles to finish before scheduling next iteration
+                    thread.join(SynchronizationDaemon.this.getFrequency() * 1000 * 3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (((SynchronizationService) SynchronizationDaemon.this.parent).isRunning()) { //the service might be stopped in between the scheduling and actual run of this job
+                    SynchronizationDaemon.this.handler.postDelayed(this, SynchronizationDaemon.this.getFrequency() * 1000);
+                }
+            }
+        }, this.getFrequency() * 1000); //this is the first run. Maybe do not delay it..
+
+    }
+
 
     public SynchronizationDaemon(Context parent) {
         this.parent = parent;
         this.cloud = new Cloud(this.parent);
+        this.handler = new Handler();
     }
 
     /**
@@ -71,8 +124,8 @@ public class SynchronizationDaemon implements Runnable {
      */
     private int getFrequency() {
         try {
-        JSONObject frequencies = new JSONObject((String)Utils.getPreference("frequencies",String.class, this.parent));
-                return frequencies.getJSONObject("synchronization").getInt("interval");
+            JSONObject frequencies = new JSONObject((String) Utils.getPreference("frequencies", String.class, this.parent));
+            return frequencies.getJSONObject("synchronization").getInt("interval");
         } catch (Exception ex) {
             Log.e(this.parent.getString(R.string.app_name), ex.getMessage() == null ? "Null pointer exception(SynchronizationDaemon.toggleData)" : ex.getMessage());
             return 180; // default to 3 mins
@@ -98,16 +151,16 @@ public class SynchronizationDaemon implements Runnable {
         }
     }
 
-    public void synchronize(SynchronizationHandler handler) {
-        String synchronizationUrl = handler.getSynchronizationURL();
-        String response = Utils.doPostHTTP(synchronizationUrl, handler.getSynchronizationData().toString());
+    public void synchronize(final SynchronizationHandler handler) {
         try {
+            String synchronizationUrl = handler.getSynchronizationURL();
+            String response = Utils.doPostHTTP(synchronizationUrl, handler.getSynchronizationData().toString());
             JSONObject responseJSON;
             responseJSON = new JSONObject(response);
             handler.processJSONResponse(responseJSON);
         } catch (Exception ex) {
-            Log.e(this.parent.getString(R.string.app_name), ex.getMessage() == null ? "Null pointer exception(SynchronizationDaemon.synchronize)" : ex.getMessage());
-
+            ex.printStackTrace();
+            Log.e(SynchronizationDaemon.this.parent.getString(R.string.app_name), ex.getMessage() == null ? "Null pointer exception(SynchronizationDaemon.synchronize)" : ex.getMessage());
         }
     }
 
