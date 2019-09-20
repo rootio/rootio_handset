84ef248c476c9421f15b3f3760d71a618dc5814a
Jude Mukundane <jude19love@gmail.com>
2019-09-20 14:47:30 +0100

    Implementation of reboot by SMS.

--- app/src/main/java/org/rootio/tools/sms/StationSMSHandler.java
+++ app/src/main/java/org/rootio/tools/sms/StationSMSHandler.java
@@ -1,23 +1,56 @@
 package org.rootio.tools.sms;
 
 import android.content.Context;
+import android.os.PowerManager;
 import android.telephony.SmsManager;
+import android.util.Log;
+
+import org.rootio.handset.BuildConfig;
+import org.rootio.handset.R;
+import org.rootio.tools.utils.Utils;
 
 public class StationSMSHandler implements MessageProcessor {
 
     private Context parent;
-    private String from;
+    private final String from;
+    private final String[] messageParts;
 
     public StationSMSHandler(Context parent, String from, String[] messageParts) {
         this.parent = parent;
         this.from = from;
+        this.messageParts = messageParts;
     }
 
     @Override
     public boolean ProcessMessage() {
-        return false;
+        if (messageParts.length != 2) {
+            return false;
+        }
 
+        // rebooting the phone
+        if (messageParts[1].equals("reboot")) {
+            try {
+                return this.reboot();
+            } catch (Exception ex) {
+                return false;
+            }
+        }
+
+        return false;
     }
+
+    private boolean reboot() {
+        try {
+            Runtime.getRuntime().exec(new String[] { "su", "-c", "reboot" });
+            return true;
+        }
+        catch(Exception ex)
+        {
+            Log.e(this.parent.getString(R.string.app_name), String.format("[StationSMSHandler.reboot] %s", ex.getMessage() == null ? "Null pointer exception" : ex.getMessage()));
+            return false;
+        }
+    }
+
 
     @Override
     public void respondAsyncStatusRequest(String from, String data) {
